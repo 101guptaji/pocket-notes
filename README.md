3926f5d87780cc0c79b67a6169a1dedb8fda19ea
Himanshu Gupta <101guptaji@gmail.com>
2025-04-04 21:51:09 +0530

    Groups Listing done

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pocket-notes
  Pocket Notes is a user-friendly app designed to help you capture and organize your notes, thoughts, tasks, and ideas on the go.
+
+
+## Features:
+1.	User should be able to create group for notes, a popup open up to create a new group and if the user clicks outside the popup then you need to close the popup
+2.	Make sure that no duplicate groups are allowed.
+3.	The display picture should consist of two letters from the group.
+4.	The user should be able to make groups, if the length of the name given to the group is less than 2
+5.	User should be able to add notes in a particular group
+6.	User should be able save the note by pressing Enter key or clicking on the Enter icon in the text input
+7.	You need to save the notes in any browser storage you find relevant for this task so that every notes and group should persist on page reload
+8.	As soon as the user changes the group, you need to fetch all the notes related to that particular group.
+9.	You also need to save meta data like date and time when the notes were saved and last updated.
+10.	DO NOT change the design of your application and keep it as close as possible to the design that has been provided to you. 
+11.	The send button only changes color when there is content in the text box, as per the design given. 
--- src/App.js
+++ src/App.js
@@ -1,43 +1,128 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import plusIcon from "./assets/plusIcon.png"
 import welcomeImg from "./assets/welcomeImg.png"
 import lockIcon from "./assets/lock.png"
 
 function App() {
-  const [groupList, setGroupList] = useState([]);
+  const [data, setData] = useState(JSON.parse(localStorage.getItem("groupNotes")) || []);
+
+  const [isModal, setIsModal] = useState(false);
+  const [errors, setErrors] = useState(null);
+
+  const colors = ['#B38BFA', '#FF79F2', '#43E6FC', '#F19576', '#0047FF', '#6691FF']
+  const [title, setTitle] = useState("");
+  const [color, setColor] = useState('')
+
+  const [selectedGrp, setSelectedGrp] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(title, color);
+
+    if (title) {
+      if (!data.find(gp => gp.id === title)) {
+        const words = title.trim().split(/\s+/);
+        // console.log("Words: ",words);
+        let initial = words.length > 1 ? words[0][0].toUpperCase() + words[1][0].toUpperCase() : words[0][0].toUpperCase();
+        setData([...data, { "id": title, "group": { id: title, title: title, initial: initial, color: color, date: new Date(Date.now()), notes: [] } }]);
+
+        setIsModal(false);
+      }
+      else {
+        setErrors("Group already exists!")
+      }
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem("groupNotes", JSON.stringify(data));
+    console.log(data);
+  }, [data]);
+
+  // clicks outside the popup then you need to close the popup
+  const closePopUp = () => {
+    if (isModal) setIsModal(false)
+
+  }
+
+
 
   return (
     <div className='app'>
-      <div className="sidemenu">
+      <div className="sidemenu" style={{ opacity: `${isModal ? '50%' : '100%'}` }} onClick={closePopUp}>
         <h1 className='heading'>Pocket Notes</h1>
         {
-          groupList &&
-          <ul>
+          data &&
+          <ul className='grp-list'>
             {
-              groupList.map((item) => {
-                <li key={item?.id}>{item.name}</li>
+              data.map((item) => {
+                return (
+                  <li key={item?.group?.id} 
+                    onClick={() => setSelectedGrp(item?.group?.id)} 
+                    style={{backgroundColor: `${selectedGrp && item?.group?.id === selectedGrp ? '#e0dcdc' : 'white'}`}}>
+                    <span style={{ backgroundColor: item?.group?.color, borderRadius: '100%' }}>{item?.group?.initial}</span>
+                    {item?.group?.title}
+                  </li>
+                )
               })
             }
           </ul>
         }
 
-        <button className='newgroup-btn'>
+        <button className='newgroup-btn' onClick={() => setIsModal(true)}>
           <img src={plusIcon} alt="create new group" />
         </button>
       </div>
 
-      <div className="welcome">
-        <img src={welcomeImg} alt="" />
-        <h2>Pocket Notes</h2>
-        <p>Send and receive messages without keeping your phone online.</p>
-        <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+      {isModal &&
+        <div className="create-group">
+          <h2>Create New group</h2>
+          <form className='form'>
+            <div className="form-row">
+              <label htmlFor="name">Group Name</label>
+              <input type="text"
+                name='name'
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder='Enter group name'
+                required />
+            </div>
+            <div className="form-row">
+              <label htmlFor="color">Choose colour</label>
+              {colors && colors.map((col, index) =>
+                <p className='color'
+                  key={index}
+                  onClick={() => setColor(col)}
+                  style={{ backgroundColor: col }} >
+                </p>
+              )}
+            </div>
+            {errors && <p style={{ color: 'red', fontSize: '10px' }}>{errors}</p>}
+            <button className='group-submit'
+              onClick={handleSubmit}>Create</button>
+          </form>
+        </div>
+      }
 
-        <p className="encryption">
-          <img src={lockIcon} alt="" />
-          <span>end-to-end encrypted</span>
-        </p>
-      </div>
+      {
+        selectedGrp ?
+          <div className="notesPage">
+
+          </div>
+          :
+          <div className="welcome" style={{ opacity: `${isModal ? '50%' : '100%'}` }} onClick={closePopUp}>
+            <img src={welcomeImg} alt="" />
+            <h2>Pocket Notes</h2>
+            <p>Send and receive messages without keeping your phone online.</p>
+            <p>Use Pocket Notes on up to 4 linked devices and 1 mobile phone</p>
+
+            <p className="encryption">
+              <img src={lockIcon} alt="" />
+              <span>end-to-end encrypted</span>
+            </p>
+          </div>
+      }
+
     </div>
   );
 }
